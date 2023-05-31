53cb31785b298b14e590b8d1ea59e496ffa58258
Maicon-g14 <maicon_gabriel12@hotmail.com>
2023-05-31 20:25:18 -0300

    Added authentication rules to recipes

--- recipes/urls.py
+++ recipes/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('', views.RecipeListView.as_view(), name="recipes-home"),
     path('recipe/<int:pk>/', views.RecipeDetailView.as_view(), name="recipe-detail"),
     path('recipe/create/', views.RecipeCreateView.as_view(), name="recipe-create"),
+    path('recipe/<int:pk>/update', views.RecipeUpdateView.as_view(), name="recipe-update"),
+    path('recipe/<int:pk>/delete', views.RecipeDeleteView.as_view(), name="recipe-delete"),
     path('about', views.about, name="recipes-about"),
 ]
