9f30ce2cd0a2ee2719256de0b4ca76cddd801434
Maicon-g14 <maicon_gabriel12@hotmail.com>
2023-05-31 00:26:04 -0300

    Added users login and registration

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'recipes'
+
+    # local apps
+    'recipes',
+    'users',
+
+    # 3rd party
+    'crispy_bootstrap4',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +131,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
