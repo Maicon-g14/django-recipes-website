c0829c51af96fd1c6ad5af2fdeda4435c3b4ee89
Maicon-g14 <maicon_gabriel12@hotmail.com>
2023-05-31 17:03:36 -0300

    Added interface to create recipes

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
 
+'app/model_viewtype'
+'recipes/recipe_detail.html'
+
 urlpatterns = [
-    path('', views.home, name="recipes-home"),
+    path('', views.RecipeListView.as_view(), name="recipes-home"),
+    path('recipe/<int:pk>/', views.RecipeDetailView.as_view(), name="recipe-detail"),
+    path('recipe/create/', views.RecipeCreateView.as_view(), name="recipe-create"),
     path('about', views.about, name="recipes-about"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from . import models
 
 
@@ -9,6 +12,22 @@ def home(request):
         'recipes': recipes
     }
     return render(request, "recipes/home.html", context)
+
+class RecipeListView(ListView):
+    model = models.Recipe
+    template_name = 'recipes/home.html'
+    context_object_name = 'recipes'
+
+class RecipeDetailView(DetailView):
+    model = models.Recipe
+
+class RecipeCreateView(LoginRequiredMixin, CreateView):
+    model = models.Recipe
+    fields = ['title', 'description']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 def about(request):
     return render(request, "recipes/about.html", {'title': 'About Us'})
